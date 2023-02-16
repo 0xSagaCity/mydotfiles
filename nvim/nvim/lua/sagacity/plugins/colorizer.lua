77e6000e696686281c6fcd870f7dc76d5fc1952b
Swaraj Pavitrakar <dragondive42@gmail.com>
2023-02-16 17:18:03 +0530

    Face palm

--- nvim/nvim/lua/sagacity/plugins/colorizer.lua
+++
@@ -1,8 +0,0 @@
--- Use the `default_options` as the second parameter, which uses
--- `foreground` for every mode. This is the inverse of the previous
--- setup configuration.
-require("colorizer").setup({
-	"*", -- Highlight all files, but customize some others.
-	css = { rgb_fn = true }, -- Enable parsing rgb(...) functions in css.
-	html = { names = false }, -- Disable parsing "names" like Blue or Gray
-})
